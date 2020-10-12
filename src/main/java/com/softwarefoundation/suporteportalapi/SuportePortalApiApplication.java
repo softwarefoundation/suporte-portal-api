14816ffabbc3d0d31ee8845f1747ce125acd7a07
Dherkyan Ribeiro <dherkyan@gmail.com>
2020-10-11 22:24:48 -0400

    projeto suporte-portal-api

---
+++ src/main/java/com/softwarefoundation/suporteportalapi/SuportePortalApiApplication.java
@@ -0,0 +1,13 @@
+package com.softwarefoundation.suporteportalapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuportePortalApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuportePortalApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/softwarefoundation/suporteportalapi/SuportePortalApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.softwarefoundation.suporteportalapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuportePortalApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
