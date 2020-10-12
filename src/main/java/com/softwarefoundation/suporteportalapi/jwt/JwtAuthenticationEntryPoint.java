20e074d124a748751fb8a49beb6327228588dc34
Dherkyan Ribeiro <dherkyan@gmail.com>
2020-10-12 12:40:03 -0400

    Add classe JwtAuthenticationEntryPoint

---
+++ src/main/java/com/softwarefoundation/suporteportalapi/jwt/JwtAuthenticationEntryPoint.java
@@ -0,0 +1,33 @@
+package com.softwarefoundation.suporteportalapi.jwt;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.softwarefoundation.suporteportalapi.config.security.SecurityConstants;
+import com.softwarefoundation.suporteportalapi.domain.HttpResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.Http403ForbiddenEntryPoint;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.OutputStream;
+
+public class JwtAuthenticationEntryPoint extends Http403ForbiddenEntryPoint {
+
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException arg2) throws IOException {
+        HttpResponse httpResponse = new HttpResponse(
+                HttpStatus.FORBIDDEN.value(),
+                HttpStatus.FORBIDDEN,
+                HttpStatus.FORBIDDEN.getReasonPhrase().toUpperCase(),
+                SecurityConstants.FORBIDDEN_MESSAGE
+        );
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+
+        OutputStream outputStream = response.getOutputStream();
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.writeValue(outputStream,httpResponse);
+        outputStream.flush();
+    }
+}
