7329bd1edde73d014c9eb03f9a910f026f7b1448
Dherkyan Ribeiro <dherkyan@gmail.com>
2020-10-12 02:08:33 -0400

    Add classe JwtTokenProvider

---
+++ src/main/java/com/softwarefoundation/suporteportalapi/jwt/JwtTokenProvider.java
@@ -0,0 +1,17 @@
+package com.softwarefoundation.suporteportalapi.jwt;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.softwarefoundation.suporteportalapi.config.security.SecurityConstants;
+import com.softwarefoundation.suporteportalapi.domain.UserPrincipal;
+import org.springframework.beans.factory.annotation.Value;
+
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.time.temporal.TemporalUnit;
+import java.util.Date;
+
+
+public class JwtTokenProvider {
+
+}
