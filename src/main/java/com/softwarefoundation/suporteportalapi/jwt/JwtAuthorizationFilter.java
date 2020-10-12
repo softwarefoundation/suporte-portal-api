4cba044944274ee3d300d52feb33f341b97d8ffd
Dherkyan Ribeiro <dherkyan@gmail.com>
2020-10-12 12:15:16 -0400

    Add classe JwtAuthorizationFilter

---
+++ src/main/java/com/softwarefoundation/suporteportalapi/jwt/JwtAuthorizationFilter.java
@@ -0,0 +1,23 @@
+package com.softwarefoundation.suporteportalapi.jwt;
+
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class JwtAuthorizationFilter extends OncePerRequestFilter {
+
+    private JwtTokenProvider jwtTokenProvider;
+
+    public JwtAuthorizationFilter(JwtTokenProvider jwtTokenProvider) {
+        this.jwtTokenProvider = jwtTokenProvider;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filter) throws ServletException, IOException {
+
+    }
+}
