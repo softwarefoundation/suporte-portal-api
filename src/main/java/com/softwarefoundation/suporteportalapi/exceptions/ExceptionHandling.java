dea5a38a741e93bd9039ca5146ff554b4d9b398a
Dherkyan Ribeiro <dherkyan@gmail.com>
2020-10-13 08:18:56 -0400

    Refatoração nome constantes mensagens

--- src/main/java/com/softwarefoundation/suporteportalapi/exceptions/ExceptionHandling.java
+++ src/main/java/com/softwarefoundation/suporteportalapi/exceptions/ExceptionHandling.java
@@ -12,17 +12,17 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class ExceptionHandling {
 
-    private static final String ACCOUNT_LOCKED = "Sua conta foi bloqueada. Contate o administrador";
-    private static final String METHOD_IS_NOT_ALLOWED = "Método não permitido. Envie '%s' request";
+    private static final String ACCOUNT_LOCKED_MSG = "Sua conta foi bloqueada. Contate o administrador";
+    private static final String METHOD_IS_NOT_ALLOWED_MSG = "Método não permitido. Envie '%s' request";
     private static final String INTERNAL_SERVER_ERROR_MSG = "Ocorreu um erro de processamento. Contate o administrador";
-    private static final String INCORRECT_CREDENTIALS = "Usuário e/ou senha incorreta. Tente novamente";
-    private static final String ACOUNT_DISABLED = "Sua conta foi desativada";
-    private static final String ERROR_PROCESSING_FILE = "Erro durante o processamento do arquivo";
-    private static final String NOT_ENOUGH_PERMISSION = "Você não tem permissão para acessar este recurso";
+    private static final String INCORRECT_CREDENTIALS_MSG = "Usuário e/ou senha incorreta. Tente novamente";
+    private static final String ACOUNT_DISABLED_MSG = "Sua conta foi desativada";
+    private static final String ERROR_PROCESSING_FILE_MSG = "Erro durante o processamento do arquivo";
+    private static final String NOT_ENOUGH_PERMISSION_MSG = "Você não tem permissão para acessar este recurso";
 
     @ExceptionHandler(DisabledException.class)
     private ResponseEntity<HttpResponse> accountDisabledException(){
-        return createHttpResponse(HttpStatus.BAD_REQUEST, ACOUNT_DISABLED);
+        return createHttpResponse(HttpStatus.BAD_REQUEST, ACOUNT_DISABLED_MSG);
     }
 
 
