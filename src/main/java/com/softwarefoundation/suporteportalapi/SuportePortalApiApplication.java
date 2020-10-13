234a359809578c095afe3f72b3de267ef27648b9
Dherkyan Ribeiro <dherkyan@gmail.com>
2020-10-12 22:43:52 -0400

    implementação do método getBCryptPasswordEncoder

--- src/main/java/com/softwarefoundation/suporteportalapi/SuportePortalApiApplication.java
+++ src/main/java/com/softwarefoundation/suporteportalapi/SuportePortalApiApplication.java
@@ -2,12 +2,19 @@ package com.softwarefoundation.suporteportalapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class SuportePortalApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SuportePortalApiApplication.class, args);
+	}
+
+	@Bean
+	public BCryptPasswordEncoder getBCryptPasswordEncoder(){
+		return new BCryptPasswordEncoder();
 	}
 
 }
