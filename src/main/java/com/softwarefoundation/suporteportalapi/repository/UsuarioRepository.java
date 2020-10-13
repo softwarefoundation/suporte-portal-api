6639b1857ccfacfa18a40e59720de86507cab37f
Dherkyan Ribeiro <dherkyan@gmail.com>
2020-10-12 21:59:18 -0400

    Add método pesquisar usuário

--- src/main/java/com/softwarefoundation/suporteportalapi/repository/UsuarioRepository.java
+++ src/main/java/com/softwarefoundation/suporteportalapi/repository/UsuarioRepository.java
@@ -4,4 +4,9 @@ import com.softwarefoundation.suporteportalapi.domain.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+
+    Usuario findUsuarioByNome(String username);
+
+    Usuario findUsuarioByEmail(String email);
+
 }
