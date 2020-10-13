a061614a5b9828a38f08366e29064107133a49ea
Dherkyan Ribeiro <dherkyan@gmail.com>
2020-10-12 23:23:32 -0400

    Add data HttpResponse

--- src/main/java/com/softwarefoundation/suporteportalapi/domain/HttpResponse.java
+++ src/main/java/com/softwarefoundation/suporteportalapi/domain/HttpResponse.java
@@ -1,7 +1,11 @@
 package com.softwarefoundation.suporteportalapi.domain;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.*;
 import org.springframework.http.HttpStatus;
+
+import java.util.Date;
+import java.util.TimeZone;
 
 @NoArgsConstructor
 @AllArgsConstructor
@@ -14,9 +18,16 @@ public class HttpResponse {
      * 200,201,400,404,500
      */
     private int httpStatusCode;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy hh:mm:ss", timezone = "America/Cuiaba")
+    private Date timestamp = new Date();
     private HttpStatus httpStatus;
     private String reason;
     private String message;
 
-
+    public HttpResponse(int httpStatusCode, HttpStatus httpStatus, String reason, String message) {
+        this.httpStatusCode = httpStatusCode;
+        this.httpStatus = httpStatus;
+        this.reason = reason;
+        this.message = message;
+    }
 }
