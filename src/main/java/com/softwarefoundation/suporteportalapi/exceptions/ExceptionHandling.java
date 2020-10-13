10df42ab66677321be2f818aa5b1ea9caed28d05
Dherkyan Ribeiro <dherkyan@gmail.com>
2020-10-13 07:47:34 -0400

    Add método accountDisabledException - controle de exceções

--- src/main/java/com/softwarefoundation/suporteportalapi/exceptions/ExceptionHandling.java
+++ src/main/java/com/softwarefoundation/suporteportalapi/exceptions/ExceptionHandling.java
@@ -1,6 +1,11 @@
 package com.softwarefoundation.suporteportalapi.exceptions;
 
+import com.softwarefoundation.suporteportalapi.domain.HttpResponse;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.DisabledException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @Slf4j
@@ -8,11 +13,27 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class ExceptionHandling {
 
     private static final String ACCOUNT_LOCKED = "Sua conta foi bloqueada. Contate o administrador";
-    private static final String METHOD_IS_NOT_ALLOWED = "Método não permitido";
+    private static final String METHOD_IS_NOT_ALLOWED = "Método não permitido. Envie '%s' request";
     private static final String INTERNAL_SERVER_ERROR_MSG = "Ocorreu um erro de processamento. Contate o administrador";
     private static final String INCORRECT_CREDENTIALS = "Usuário e/ou senha incorreta. Tente novamente";
     private static final String ACOUNT_DISABLED = "Sua conta foi desativada";
     private static final String ERROR_PROCESSING_FILE = "Erro durante o processamento do arquivo";
     private static final String NOT_ENOUGH_PERMISSION = "Você não tem permissão para acessar este recurso";
+
+    @ExceptionHandler(DisabledException.class)
+    private ResponseEntity<HttpResponse> accountDisabledException(){
+        return createHttpResponse(HttpStatus.BAD_REQUEST, ACOUNT_DISABLED);
+    }
+
+
+    private ResponseEntity<HttpResponse> createHttpResponse(HttpStatus httpStatus, String message){
+        HttpResponse httpResponse = new HttpResponse(httpStatus.value(),
+                httpStatus,
+                httpStatus.getReasonPhrase().toUpperCase(),
+                message.toUpperCase());
+
+        return new ResponseEntity<>(httpResponse,httpStatus);
+    }
+
 
 }
