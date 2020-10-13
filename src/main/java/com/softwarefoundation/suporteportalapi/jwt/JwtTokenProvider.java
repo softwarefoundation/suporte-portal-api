f2d49353b9ef07d0cdf33064d83a594159711137
Dherkyan Ribeiro <dherkyan@gmail.com>
2020-10-12 22:49:32 -0400

    Add anotação @Component

--- src/main/java/com/softwarefoundation/suporteportalapi/jwt/JwtTokenProvider.java
+++ src/main/java/com/softwarefoundation/suporteportalapi/jwt/JwtTokenProvider.java
@@ -12,6 +12,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.ArrayList;
@@ -20,7 +21,7 @@ import java.util.Date;
 import java.util.List;
 import java.util.stream.Collectors;
 
-
+@Component
 public class JwtTokenProvider {
 
     @Value("${jwt.secret}")
