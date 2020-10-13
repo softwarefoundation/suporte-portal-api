299bc235e5acc83f16000727719177b931a98990
Dherkyan Ribeiro <dherkyan@gmail.com>
2020-10-12 22:05:53 -0400

    Add classe SecurityConfiguration

---
+++ src/main/java/com/softwarefoundation/suporteportalapi/config/security/SecurityConfiguration.java
@@ -0,0 +1,9 @@
+package com.softwarefoundation.suporteportalapi.config.security;
+
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+
+
+}
